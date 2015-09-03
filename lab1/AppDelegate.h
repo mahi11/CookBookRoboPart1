2ec1e253471e24237acaef8177bd8f763f57d7ef
Dhanthuluri <mdc87@kc-sce463lab-14.kc.umkc.edu>
2015-09-03 18:54:39 -0500

    Initial Commit

---
+++ lab1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lab1
+//
+//  Created by Dhanthuluri, Mahidhar Varma (UMKC-Student) on 9/3/15.
+//  Copyright (c) 2015 Dhanthuluri, Mahidhar Varma (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
